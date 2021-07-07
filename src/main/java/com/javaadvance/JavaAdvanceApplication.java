7322a88078ed8df2ff594e8ad7554f7f5b7db04a
BohdanKravets <bodyakravets@gmail.com>
2021-07-07 16:19:31 +0300

    master

---
+++ src/main/java/com/javaadvance/JavaAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.javaadvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAdvanceApplication.class, args);
+	}
+
+}
